cdd12a890e5b8425722877ae707d0fdbb24d28db
Yakiv Lobach <lobachyakiv@gmail.com>
2024-08-31 17:14:08 +0300

    fourth_try

--- db/models.py
+++ db/models.py
@@ -26,8 +26,16 @@ class CinemaHall(models.Model):
 
 class MovieSession(models.Model):
     show_time = models.DateTimeField()
-    cinema_hall = models.ForeignKey(CinemaHall, on_delete=models.CASCADE, related_name="sessions")
-    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name="sessions")
+    cinema_hall = models.ForeignKey(
+        CinemaHall,
+        on_delete=models.CASCADE,
+        related_name="sessions"
+    )
+    movie = models.ForeignKey(
+        Movie,
+        on_delete=models.CASCADE,
+        related_name="sessions"
+    )
 
     def __str__(self) -> str:
         return (f"{self.movie.title} "
